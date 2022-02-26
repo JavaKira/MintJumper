6dbd1a3ae58b9e65d40b89667294e20fe735af97
JavaKira <JavaKira@yandex.ru>
2022-02-26 17:40:06 +0300

    Added PurseObject

---
+++ Assets/Scripts/PurseObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PurseObject : MonoBehaviour
+{
+    [SerializeField] private GameObject pursedObject;
+    [SerializeField] private float softness;
+
+    private void FixedUpdate()
+    {
+        var distance = transform.position - pursedObject.transform.position;
+        transform.Translate(-(Vector2) distance / softness);
+    }
+}
