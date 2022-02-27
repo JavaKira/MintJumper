10e762659b43a0bd693782ce193833f3af06c374
JavaKira <JavaKira@yandex.ru>
2022-02-27 12:16:34 +0300

    Added MainMenu scene

---
+++ Assets/Scripts/SceneOpenButtonBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SceneOpenButtonBehaviour : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(sceneName);
+        });
+    }
+}
