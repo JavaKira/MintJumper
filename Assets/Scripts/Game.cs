952d7fa532a480f918f6bc533a57053cf48ae7a8
JavaKira <JavaKira@yandex.ru>
2022-03-01 18:53:27 +0300

    Added Game

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game Instance;
+
+    private bool _pause;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    public void Pause()
+    {
+        _pause = true;
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        _pause = false;
+        Time.timeScale = 1;
+    }
+
+    public bool IsPause()
+    {
+        return _pause;
+    }
+}
