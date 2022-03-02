4ba1bd375637fe3e6f5927845ace76aaa96d3065
JavaKira <JavaKira@yandex.ru>
2022-03-02 22:09:08 +0300

    Added OnTriggerEnter2DCheck

---
+++ Assets/Scripts/OnTriggerEnter2DCheck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEnter2DCheck : MonoBehaviour
+{
+    [SerializeField] private GameObject waitedObject;
+    
+    public UnityEvent onDone = new UnityEvent();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.Equals(waitedObject))
+            onDone.Invoke();
+    }
+}
