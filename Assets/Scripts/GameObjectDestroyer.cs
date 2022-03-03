c0d364c218ec617f1792e2006c4798408545bca7
JavaKira <JavaKira@yandex.ru>
2022-03-03 20:09:01 +0300

    Added GameObjectDestroyer

---
+++ Assets/Scripts/GameObjectDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameObjectDestroyer : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToDelete;
+    
+    public void Destroy()
+    {
+        Destroy(objectToDelete);
+    }
+}
