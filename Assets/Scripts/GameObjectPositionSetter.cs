01009fbd2cae96d6e137e339334eafef532d25e0
JavaKira <JavaKira@yandex.ru>
2022-03-04 18:07:58 +0300

    Added GameObjectPositionSetter

---
+++ Assets/Scripts/GameObjectPositionSetter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameObjectPositionSetter : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSet;
+    [SerializeField] private Transform position;
+
+    public void Set()
+    {
+        var position1 = position.position;
+        objectToSet.transform.position = new Vector3(position1.x, position1.y, position1.z);
+    }
+}
