0c2020cf5ab79cb641b3377bb061d2aa856dca94
JavaKira <JavaKira@yandex.ru>
2022-03-04 18:08:31 +0300

    Added BeetleBehaviour

---
+++ Assets/Scripts/BeetleBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BeetleBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speedMultiplier;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(1 * speedMultiplier * Time.fixedDeltaTime, 0, 0);
+    }
+}
