55de68e19c811cbe9f7f1083f65d5b7008a35db5
JavaKira <JavaKira@yandex.ru>
2022-03-04 18:07:15 +0300

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float time;
+    [SerializeField] private bool startAtAwake;
+    
+    public UnityEvent onDone = new UnityEvent();
+
+    private void Awake()
+    {
+        if (startAtAwake)
+            StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(Start());
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        onDone.Invoke();
+    }
+}
