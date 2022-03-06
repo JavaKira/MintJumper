dccbc8891be7036c9995066833fc02ca3cd0a0fe
JavaKira <JavaKira@yandex.ru>
2022-03-06 10:00:53 +0300

    Added Mob and Beetle prefab

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+    [SerializeField] private float maxHealth;
+
+    private float _health;
+    
+    private void Awake()
+    {
+        Heal();
+    }
+
+    private void Heal()
+    {
+        _health = maxHealth;
+    }
+
+    private void ApplyDamage(float damage)
+    {
+        _health -= damage;
+        
+        if (_health <= 0)
+            Dead();
+    }
+
+    private void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
