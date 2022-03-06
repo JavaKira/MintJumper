5d463e87eb448ce879e843f40a6dadb73f68dbb8
JavaKira <JavaKira@yandex.ru>
2022-03-06 10:23:08 +0300

    Added ShootButton behaviour and icon

---
+++ Assets/Scripts/ShootButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootButtonBehaviour : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private WeaponSlot slot;
+    
+    private bool _touched;
+
+    private void FixedUpdate()
+    {
+        if (_touched)
+            slot.Weapon.Attack();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _touched = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _touched = false;
+    }
+}
