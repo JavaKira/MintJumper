d0cc9b46d51a880db8a02b8aeb0d46b3c46fe4fb
JavaKira <JavaKira@yandex.ru>
2022-03-08 12:07:27 +0300

    Added MissionEndPanel

---
+++ Assets/Scripts/MissionEndPanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissionEndPanel : MonoBehaviour
+{
+    public void Back()
+    {
+        Game.Instance.Resume();
+        SceneManager.LoadScene("CampaignRoadScene");
+    }
+}
