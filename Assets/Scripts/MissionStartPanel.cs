205b828ff8a0ddbbcdaaf0cf4b6a3587c361485e
JavaKira <JavaKira@yandex.ru>
2022-03-15 22:36:16 +0300

    Added MissionStartPanel

---
+++ Assets/Scripts/MissionStartPanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MissionStartPanel : MonoBehaviour
+{
+    public static CampaignPoint.CampaignPointData LastPointData;
+
+    public void Open(CampaignPoint point)
+    {
+        LastPointData = point.Data;
+    }
+}
