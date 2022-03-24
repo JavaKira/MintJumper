613937e67afec2be273770e9cf2cc46feb4b7dbb
JavaKira <JavaKira@yandex.ru>
2022-03-24 23:13:08 +0300

    Added KeyListener

---
+++ Assets/Scripts/KeyListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyListener : MonoBehaviour
+{
+    [SerializeField] private KeyCode keyCode;
+    
+    public UnityEvent onKeyDown = new UnityEvent();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+            onKeyDown.Invoke();
+    }
+}
