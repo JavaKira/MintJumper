fc749a9484a694d6fa174451e2f57fb5611e9f8d
JavaKira <JavaKira@yandex.ru>
2022-03-25 11:42:32 +0300

    Move weapon to resources folder and Added Weapon GetByName()

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -5,12 +5,17 @@ namespace Weapon
     public abstract class Weapon : MonoBehaviour
     {
         protected Mob Owner;
-    
+
         public abstract void Attack();
-    
+
         public void SetOwner(Mob mob)
         {
             Owner = mob;
+        }
+
+        public static Weapon GetByName(string name)
+        {
+            return Resources.Load<Weapon>(name);
         }
     }
 }
