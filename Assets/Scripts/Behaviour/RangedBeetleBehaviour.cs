1a958edd7dd073cc8eee0edf6f481a7b7f0f7539
JavaKira <JavaKira@yandex.ru>
2022-03-27 20:27:50 +0300

    Added RangedBeetle and RangedBeetleBehaviour

---
+++ Assets/Scripts/Behaviour/RangedBeetleBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Behaviour
+{
+    public class RangedBeetleBehaviour : BeetleBehaviour
+    {
+        [SerializeField] private Weapon.Weapon weapon;
+        private Mob _mob;
+
+        private void Awake()
+        {
+            _mob = GetComponent<Mob>();
+        }
+
+        protected override void FixedUpdateNoIdle()
+        {
+            var transform1 = transform;
+            var direction = -(transform1.position - Target.transform.position).normalized.x;
+            var scale = transform1.localScale;
+            if (direction > 0)
+                transform.localScale = new Vector3(1, scale.y, scale.z);
+        
+            if (direction < 0)    
+                transform.localScale = new Vector3(-1, scale.y, scale.z);
+            
+            weapon.SetOwner(_mob);
+            weapon.Attack();
+        }
+    }
+}
