176edb32f2a0af0501d5693acf9dac407e7e2e37
JavaKira <JavaKira@yandex.ru>
2022-04-02 23:04:48 +0300

    Added Boss and BossHealthBar

---
+++ Assets/ScriptableObjects/Boss.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Behaviour;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [RequireComponent(typeof(Mob))]
+    public class Boss : MonoBehaviour
+    {
+        public Mob Mob { get; private set; }
+
+        private void Awake()
+        {
+            Mob = GetComponent<Mob>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.GetComponent<PlayerBehaviour>() == null) return;
+            FindObjectOfType<BossHealthBar>(true).Open(this);
+            Mob.healthChanged.AddListener(MobHealthChanged);
+        }
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.GetComponent<PlayerBehaviour>() != null)
+                FindObjectOfType<BossHealthBar>(true).Close();
+        }
+
+        private void MobHealthChanged(float health)
+        {
+            if (health <= 0)
+            {
+                FindObjectOfType<BossHealthBar>(true).Close(); 
+            }
+        }
+
+        private void OnDisable()
+        {
+            Mob.healthChanged.RemoveListener(MobHealthChanged);
+        }
+    }
+}
---
+++ Assets/Scripts/BossHealthBar.cs
@@ -0,0 +1,31 @@
+﻿using ScriptableObjects;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthBar : MonoBehaviour
+{
+    [SerializeField] private Image progressBarImage;
+    [SerializeField] private TMP_Text title;
+    
+    private Mob _lastMob;
+
+    public void Open(Boss boss)
+    {
+        gameObject.SetActive(true);
+        _lastMob = boss.Mob;
+        title.text = boss.Mob.name;
+        boss.Mob.healthChanged.AddListener(UpdateHealth);
+    }
+
+    private void UpdateHealth(float health)
+    {
+        progressBarImage.fillAmount = health / _lastMob.MaxHealth;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        _lastMob.healthChanged.RemoveListener(UpdateHealth);
+    }
+}
